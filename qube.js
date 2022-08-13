a0883d3373890ada9d3cffa9bea7064450bdc968
manthrax <manthrax@gmail.com>
2022-08-12 21:27:45 -0700

    Fix click and drag.

--- qube.js
+++ qube.js
@@ -5,6 +5,12 @@ export default function Qube(THREE, scene, loader) {
     let qube;
     let imesh;
     let blks = [];
+
+    let status = document.createElement('span');
+    status.innerText = "QUBE!"
+    status.style = "color:white;position:absolute;left:30px;top:30px;font-family:Arial"
+    document.body.appendChild(status)
+
     let rbk = glb=>{
         let m = glb.scene.children[2];
         let box = m;
@@ -12,7 +18,6 @@ export default function Qube(THREE, scene, loader) {
         qube = this.root = new Object3D();
         let nr = 1;
         let nstep = 1;
-        imesh = new InstancedMesh(m.geometry,m.material,(nr * 2 + 1) ** 2);
         for (let i = -nr, bx; i <= nr; i += nstep)
             for (let j = -nr; j <= nr; j += nstep)
                 for (let k = -nr; k <= nr; k += nstep) {
@@ -21,10 +26,11 @@ export default function Qube(THREE, scene, loader) {
                     blks.push(bx);
                 }
         qube.scale.multiplyScalar(2);
-        qube.traverse(e=>e.isMesh && (e.castShadow = e.receiveShadow = true));
+        qube.traverse(e=>{
+            e.isMesh && (e.castShadow = e.receiveShadow = true);
+            if(e.material){e.material.metalness=.8;e.material.roughness=.2;}});
         qube.updateMatrix(true)
         scene.add(qube);
-        //scene.add(imesh)
     }
     ;
     new loader().load(`rbik.glb`, rbk);
@@ -32,11 +38,11 @@ export default function Qube(THREE, scene, loader) {
     let tv0 = new Vector3();
 
     let isSolved = ()=>{
-        let dist=0;
+        let dist = 0;
         tv0.copy(qube.children[0].rotation);
-        qube.children.forEach(e=>tv1.copy(e.rotation) && (dist+=tv0.distanceToSquared(tv1)))
-        console.log('-----',dist)
-        return dist<.0001;
+        qube.children.forEach(e=>tv1.copy(e.rotation) && (dist += tv0.distanceToSquared(tv1)))
+        console.log('-----', dist)
+        return dist < .0001;
     }
     let sortSlabs = ()=>{
         qube.children.forEach(e=>{
@@ -49,7 +55,7 @@ export default function Qube(THREE, scene, loader) {
             loc.z = floor(loc.z + 0.5);
             e.position.copy(loc);
             loc.add(tv0.set(1, 1, 1));
-            (!e.userData.solvedLoc) && (e.userData.solvedLoc=loc.clone());
+            (!e.userData.solvedLoc) && (e.userData.solvedLoc = loc.clone());
         }
         );
     }
@@ -74,8 +80,6 @@ export default function Qube(THREE, scene, loader) {
     let rotating = false;
     let tv1 = tv0.clone();
 
-
-
     this.rotateSlab = s=>{
         if (rotating)
             return;
@@ -85,6 +89,7 @@ export default function Qube(THREE, scene, loader) {
         let row = ((s % 6) / 2) | 0;
         let dir = s & 1;
         console.log("face", face, "row", row, "dir", dir);
+
         let c = qube.children;
         let slab = getSlab(face, row);
         rotor.rotation.set(0, 0, 0);
@@ -112,17 +117,150 @@ export default function Qube(THREE, scene, loader) {
                 rotor.rotation.set(rotarget.x, rotarget.y, rotarget.z, "XYZ");
                 c.forEach(k=>qube.attach(k));
 
-            sortSlabs();
-            let solved = isSolved()
-            let i = document.getElementsByTagName('title')[0]
-            i &&( i.innerText = solved?'SOLVED':"UNSOLVED")                
+                sortSlabs();
+                let solved = isSolved()
+                let i = status
+                i && (i.innerText += solved ? '      SOLVED' : "     UNSOLVED")
 
             }
         }
-        blks.forEach((e,i)=>{
-            e.updateMatrixWorld();
-            imesh.setMatrixAt(i, e.matrixWorld);
-        }
-        )
     }
+let v0 = new THREE.Vector3()
+let v1 = new THREE.Vector3()
+let v2 = new THREE.Vector3()
+    let {abs}=Math;
+    let toSide = v0.clone().set(12,6,0)        
+        
+    let maxAxis=(v0)=>{
+        v1.set(abs(v0.x),abs(v0.y),abs(v0.z));
+        let ax='x'
+        if(v1.y>v1.x)ax='y'
+        if(v1.z>v1[ax])ax='z';
+        return ax;
+    }
+    let idx = 0;
+    let actionCode;
+    this.drag = (startHits,endHits)=>{
+        if(rotating) return;
+        let start=startHits[0].point;
+        let end=endHits[0].point
+        v0.copy(start)
+        let faceAxis = maxAxis(v0);
+        let pos=(v0[faceAxis]>0)?1:0;
+        status.innerText = 'face:'+(pos?'+':'-')+faceAxis
+        let code = pos?faceAxis.toUpperCase():faceAxis;
+        v0.copy(end).sub(start);
+        let dragAxis = maxAxis(v0);
+        let dpos=(v0[dragAxis]>0)?1:0;
+        status.innerText += ' dragDir:'+(dpos?'+':'-')+dragAxis
+        code += dpos?dragAxis.toUpperCase():dragAxis;
+        v0.copy(start)
+        v0[faceAxis]=0;
+        v0[dragAxis]=0;
+        let slab=v0.x+v0.y+v0.z;
+        slab = ((slab+3)/2)|0;
+        code+=slab;
+        status.innerText += ' slab:'+slab
+        actionCode = code;
+        let ud = undefined;
+        let moveMap={
+            Xz0:6,
+            XZ0:7,
+            Xz1:8,
+            XZ1:9,
+            Xz2:10,
+            XZ2:11,
+            XY0:12,
+            Xy0:13,
+            XY1:14,
+            Xy1:15,
+            XY2:16,
+            Xy2:17,
+
+            xz0:7,
+            xZ0:6,
+            xz1:9,
+            xZ1:8,
+            xz2:11,
+            xZ2:10,
+            xY0:13,
+            xy0:12,
+            xY1:15,
+            xy1:14,
+            xY2:17,
+            xy2:16,
+
+
+            Zx0:7,
+            ZX0:6,
+            Zx1:9,
+            ZX1:8,
+            Zx2:11,
+            ZX2:10,
+            ZY0:1,
+            Zy0:0,
+            ZY1:3,
+            Zy1:2,
+            ZY2:5,
+            Zy2:4,
+
+            
+            zx0:6,
+            zX0:7,
+            zx1:8,
+            zX1:9,
+            zx2:10,
+            zX2:11,
+            zY0:0,
+            zy0:1,
+            zY1:2,
+            zy1:3,
+            zY2:4,
+            zy2:5,
+
+            
+            
+            Yx0:12,
+            YX0:13,
+            Yx1:14,
+            YX1:15,
+            Yx2:16,
+            YX2:17,
+            YZ0:0,
+            Yz0:1,
+            YZ1:2,
+            Yz1:3,
+            YZ2:4,
+            Yz2:5,
+
+            yx0:13,
+            yX0:12,
+            yx1:15,
+            yX1:14,
+            yx2:17,
+            yX2:16,
+            yZ0:1,
+            yz0:0,
+            yZ1:3,
+            yz1:2,
+            yZ2:5,
+            yz2:4,
+
+        }
+        let moveId = moveMap[code] 
+
+        if(moveId!==undefined)
+            this.rotateSlab(moveId);
+        status.innerText += ' code:'+code + ' moveID:' + moveId
+        /*
+        */
+    }
+        document.addEventListener('keydown',e=>{
+            if(e.code=='Space'){
+                idx %= 18;
+                status.innerText = actionCode + ' id:'+idx
+                this.rotateSlab(idx);//side+dir);
+                idx++;
+            }
+        })
 }
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import*as THREE from "https://threejs.org/build/three.module.js";
 import {OrbitControls} from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 import {GLTFLoader} from "https://threejs.org/examples/jsm/loaders/GLTFLoader.js";
+import {RGBELoader} from "https://threejs.org/examples/jsm/loaders/RGBELoader.js";
 
 import Qube from "./qube.js";
-import MouseRaycaster from "../core/js/rendering/MouseRaycaster.js";
-import ZoomToCursorBehavior from "../core/js/rendering/ZoomToCursorBehavior.js";
+//import MouseRaycaster from "../core/js/rendering/camera2/MouseRaycaster.js";
+//import ZoomToCursorBehavior from "../core/js/rendering/camera2/ZoomToCursorBehavior.js";
 
 const camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(1, 1, 1).multiplyScalar(8);
@@ -14,26 +15,43 @@ const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
     antialias: true
 });
+
+/*
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.VSMShadowMap;
+*/
+
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
-
+renderer.setClearColor('white')
 const controls = new OrbitControls(camera,renderer.domElement);
 //controls.autoRotate= true
 controls.autoRotateSpeed = 1;
-//controls.enablePan = false;
+controls.enablePan = false;
 
-
-new ZoomToCursorBehavior({THREE,controls,camera});
+let envMaps=['evening_meadow_1k.hdr','pretville_street_1k (1).hdr','solitude_interior_1k.hdr']
+function loadHDREquirect(path) {
+    var pmremGenerator = new THREE.PMREMGenerator(renderer);
+    pmremGenerator.compileEquirectangularShader();
+    new RGBELoader().setPath('').load(path,
+    function(texture) {
+        var envMap = pmremGenerator.fromEquirectangular(texture).texture;
+        scene.background = envMap;
+        scene.environment = envMap;
+        texture.dispose();
+        pmremGenerator.dispose();
+    })
+}
+let arnd =(a)=>a[(a.length*Math.random())|0]
+loadHDREquirect('./'+envMaps[2])//arnd(envMaps));//pretville_street_1k (1).hdr')
+//solitude_interior_1k.hdr
 
 const clock = new THREE.Clock();
 
 onWindowResize();
 window.addEventListener("resize", onWindowResize, false);
-
+/*
 const lights = new THREE.Object3D();
-
 let light = new THREE.DirectionalLight("white",1.1);
 light.shadow.enabled = true;
 light.position.set(1, 1, 1).multiplyScalar(100);
@@ -55,20 +73,20 @@ dirLight.castShadow = true;
 
 let light1 = new THREE.PointLight(0xffffff);
 light1.position.set(5, 9, -5).multiplyScalar(100);
-
-let light2 = new THREE.PointLight(0xffffff);
-light2.position.set(-1, -1, -1).multiplyScalar(100);
-light2.position.set(0, 0, 1)
+*/
+let light2 = new THREE.PointLight(0xffffff,100,10,2);
+light2.position.set(0, 0, .1)
 camera.add(light2)
-scene.add(lights);
+//scene.add(lights);
+
 scene.add(camera);
 
 let qube = new Qube(THREE,scene,GLTFLoader)
 
 function render() {
+    qube.update()
     controls.update();
     renderer.render(scene, camera);
-    qube.update()
 }
 
 document.addEventListener("keydown", qube.handleKey)
@@ -82,45 +100,94 @@ function onWindowResize(event) {
 }
 
 function animate() {
-    requestAnimationFrame(animate);
     render();
+}
+
+let dragStartHits;
+
+let hitIndicator = new THREE.Mesh(new THREE.PlaneGeometry(.5,.5),new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide
+}))
+hitIndicator.geometry.translate(0, 0, .01)
+hitIndicator.renderOrder = 2;
+hitIndicator.material.depthFunc = THREE.GreaterDepth;
+hitIndicator.onBeforeRender = function() {
+    if (!dragStartHits)
+        this.scale.multiplyScalar(.98)
+    if (this.scale.x < .001)
+        this.parent.remove(this);
+}
+let dragIndicator = hitIndicator.clone();
+let v0 = new THREE.Vector3()
+let v1 = new THREE.Vector3()
+let v2 = new THREE.Vector3()
+let normalMatrix = new THREE.Matrix3()
+let showHit = (hit,hitIndicator)=>{
+    if (hit.object != hitIndicator) {
+        //scene.traverse(e=>e.isMesh&&(e.material.wireframe=true))
+        hitIndicator.scale.set(1, 1, 1)
+        hitIndicator.position.copy(hit.point)
+        scene.add(hitIndicator);
+        normalMatrix.getNormalMatrix(hit.object.matrixWorld);
+
+        v0.copy(hit.face.normal).applyMatrix3(normalMatrix).normalize();
+        v1.copy(hitIndicator.position).sub(v0)
+        hitIndicator.lookAt(v1)
+    }
+}
+
+export default function MouseRaycaster(THREE) {
+    const mouse = new THREE.Vector2();
+    let raycaster = new THREE.Raycaster()
+    this.raycast = ({event, camera, root, recursive=true})=>{
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        raycaster.setFromCamera(mouse, camera)
+        if (typeof root === 'array')
+            return raycaster.intersectObjects(root, recursive);
+        return raycaster.intersectObject(root, recursive);
+    }
 }
 
 let mouseRaycaster = new MouseRaycaster(THREE)
 
-let hitIndicator = new THREE.Mesh(new THREE.SphereGeometry(.1),new THREE.MeshBasicMaterial())
-let hi1=hitIndicator.clone()
-hitIndicator.scale.set(1,1,1)
-hitIndicator.add(hi1)
-hi1.material=hi1.material.clone()
-hi1.material.color.set('black')
-hi1.renderOrder = 2;
-hi1.material.depthFunc=THREE.GreaterDepth;
-hi1.scale.multiplyScalar(.5)
-hitIndicator.onBeforeRender=function(){
-    hitIndicator.scale.multiplyScalar(.98)
-    if(hitIndicator.scale.x<.001)hitIndicator.parent.remove(hitIndicator);
-}
-let showHit=(obj,point)=>{
-    if(obj!=hitIndicator){
-        hitIndicator.scale.set(1,1,1)
-        obj.add(hitIndicator)
-        hitIndicator.position.copy(point)
-    }
-}
-
-
-document.addEventListener('mousedown',function(event){
+let doRaycast = ()=>{
     let hits = mouseRaycaster.raycast({
         event,
         camera,
-        root:qube.root,
-        recursive:true})
-    console.log(hits)
-    if(hits.length){
-        let h=hits[0];
-        showHit(h.object,h.object.worldToLocal(h.point))
+        root: qube.root,
+        recursive: true
+    })
+    //console.log(hits)
+    if (hits.length) {
+        return hits;
     }
-})
+}
 
-animate();
+function pup(event) {
+    let dragEndHits = doRaycast()
+    if (dragEndHits) {
+        qube.drag(dragStartHits, dragEndHits)
+    }
+    dragStartHits = dragEndHits = undefined;
+    controls.enabled = true;
+    document.removeEventListener('pointermove', pmove)
+    document.removeEventListener('pointerup', pup)
+}
+
+function pmove(event) {
+    let dragMoveHits = doRaycast()
+    if (dragMoveHits)
+        showHit(dragMoveHits[0], dragIndicator)
+}
+function pdown(event) {
+    if (dragStartHits = doRaycast()) {
+        showHit(dragStartHits[0], hitIndicator)
+        controls.enabled = false;
+        document.addEventListener('pointermove', pmove)
+        document.addEventListener('pointerup', pup)
+    }
+}
+document.addEventListener('pointerdown', pdown)
+
+renderer.setAnimationLoop(animate);
